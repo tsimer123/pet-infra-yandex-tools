0f04aa6505bbc3a665e4608d7a02af29e973768b
Roman Martsev <matrixur@gmail.com>
2023-03-31 06:34:20 +0000

    chore(github) add first config

--- go.mod
+++ go.mod
@@ -5,4 +5,20 @@ go 1.20
 require (
 	github.com/caarlos0/env/v6 v6.10.1
 	github.com/golang-jwt/jwt/v4 v4.5.0
+	github.com/google/go-github/v45 v45.2.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+)
+
+require (
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/sirupsen/logrus v1.9.0
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5
+	golang.org/x/oauth2 v0.6.0
 )
---
+++ internal/github/github.go
@@ -0,0 +1,120 @@
+package location
+
+import (
+	"context"
+	crypto_rand "crypto/rand"
+	"encoding/base64"
+	"fmt"
+
+	"github.com/google/go-github/v45/github"
+	"github.com/sirupsen/logrus"
+	"golang.org/x/crypto/nacl/box"
+	"golang.org/x/oauth2"
+)
+
+// GitHubActionsService type
+type GitHubActionsService interface {
+	GetRepoPublicKey(context.Context, string, string) (*github.PublicKey, *github.Response, error)
+	CreateOrUpdateRepoSecret(context.Context, string, string, *github.EncryptedSecret) (*github.Response, error)
+}
+
+// GitHub type
+type GitHub struct {
+	Base64Decode bool
+	KeyIDEnvVar  string
+	KeyEnvVar    string
+	Owner        string
+	Repo         string
+}
+
+func UpdateSecret(token, owner, repo, secretName, secretValue string) {
+	ctx, client, err := githubAuth(token)
+	if err != nil {
+		logrus.Fatalf("Unable to authenticate to GitHub: %v", err)
+	}
+
+	actionsService := client.Actions
+	if err := addRepoSecret(ctx, actionsService, owner, repo, secretName, secretValue); err != nil {
+		logrus.Fatalf("Unable to add secret to GitHub: %v", err)
+	}
+}
+
+// githubAuth returns a GitHub client and context.
+func githubAuth(token string) (context.Context, *github.Client, error) {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+	return ctx, client, nil
+}
+
+// addRepoSecret will add a secret to a GitHub repo for use in GitHub Actions.
+//
+// Finally, the secretName and secretValue will determine the name of the secret added and it's corresponding value.
+//
+// The actual transmission of the secret value to GitHub using the api requires that the secret value is encrypted
+// using the public key of the target repo. This encryption is done using x/crypto/nacl/box.
+//
+// First, the public key of the repo is retrieved. The public key comes base64
+// encoded, so it must be decoded prior to use.
+//
+// Second, the decode key is converted into a fixed size byte array.
+//
+// Third, the secret value is converted into a slice of bytes.
+//
+// Fourth, the secret is encrypted with box.SealAnonymous using the repo's decoded public key.
+//
+// Fifth, the encrypted secret is encoded as a base64 string to be used in a github.EncodedSecret type.
+//
+// Sixt, The other two properties of the github.EncodedSecret type are determined. The name of the secret to be added
+// (string not base64), and the KeyID of the public key used to encrypt the secret.
+// This can be retrieved via the public key's GetKeyID method.
+//
+// Finally, the github.EncodedSecret is passed into the GitHub client.Actions.CreateOrUpdateRepoSecret method to
+// populate the secret in the GitHub repo.
+func addRepoSecret(ctx context.Context, actionsService GitHubActionsService, owner string, repo, secretName string, secretValue string) error {
+	publicKey, _, err := actionsService.GetRepoPublicKey(ctx, owner, repo)
+	if err != nil {
+		return err
+	}
+
+	encryptedSecret, err := encryptSecretWithPublicKey(publicKey, secretName, secretValue)
+	if err != nil {
+		return err
+	}
+
+	if _, err := actionsService.CreateOrUpdateRepoSecret(ctx, owner, repo, encryptedSecret); err != nil {
+		return fmt.Errorf("Actions.CreateOrUpdateRepoSecret returned error: %v", err)
+	}
+
+	logrus.Infof("Added GitHub secret: %s to %s/%s", secretName, owner, repo)
+
+	return nil
+}
+
+func encryptSecretWithPublicKey(publicKey *github.PublicKey, secretName string, secretValue string) (*github.EncryptedSecret, error) {
+	decodedPublicKey, err := base64.StdEncoding.DecodeString(publicKey.GetKey())
+	if err != nil {
+		return nil, fmt.Errorf("base64.StdEncoding.DecodeString was unable to decode public key: %v", err)
+	}
+
+	var boxKey [32]byte
+	copy(boxKey[:], decodedPublicKey)
+	secretBytes := []byte(secretValue)
+	encryptedBytes, err := box.SealAnonymous([]byte{}, secretBytes, &boxKey, crypto_rand.Reader)
+	if err != nil {
+		return nil, fmt.Errorf("box.SealAnonymous failed with error %w", err)
+	}
+
+	encryptedString := base64.StdEncoding.EncodeToString(encryptedBytes)
+	keyID := publicKey.GetKeyID()
+	encryptedSecret := &github.EncryptedSecret{
+		Name:           secretName,
+		KeyID:          keyID,
+		EncryptedValue: encryptedString,
+	}
+	return encryptedSecret, nil
+}
